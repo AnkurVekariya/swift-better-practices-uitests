0bd1d08b37ea61005ce3a58cc085fe231f8c188b
AnkurVekariya <ankurvekariya2@gmail.com>
2022-01-10 13:13:25 +0530

    created mac framwork
    created mca framwork to build network layer.

---
+++ BlogLoader/BlogLoader/BlogItem.swift
@@ -0,0 +1,15 @@
+//
+//  BlogItem.swift
+//  BlogLoader
+//
+//  Created by Ankur Vekaria on 10/01/22.
+//
+
+import Foundation
+
+struct BlogItem {
+    let id: UUID
+    let Description: String?
+    let Location: String?
+    let image: URL
+}
---
+++ BlogLoader/BlogLoader/BlogLoader.h
@@ -0,0 +1,18 @@
+//
+//  BlogLoader.h
+//  BlogLoader
+//
+//  Created by Ankur Vekaria on 10/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlogLoader.
+FOUNDATION_EXPORT double BlogLoaderVersionNumber;
+
+//! Project version string for BlogLoader.
+FOUNDATION_EXPORT const unsigned char BlogLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlogLoader/PublicHeader.h>
+
+
